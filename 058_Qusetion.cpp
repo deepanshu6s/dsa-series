ee58f1159f5a791f366bc8996984d15335993d2c
deepanshu s <deepanshu6sharawat@gmail.com>
2026-05-12 22:49:32 +0530

    added 058 lec 13 question no 1

---
+++ 058_Qusetion.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+int first(int arr[], int size , int key){
+    int start = 0;
+    int end = size-1;
+    int mid = start + (end-start)/2;
+    int ans = -1;
+
+    while(start <= end){
+        if(arr[mid] == key){
+            ans = mid;
+            end = mid-1;
+        }else if (arr[mid] < key){
+            start = mid+1;
+        }else if(arr[mid] > key){
+            end = mid-1;
+        }
+        mid = start + (end-start)/2;
+    }
+    return ans;
+}
+
+
+int last(int arr[], int size , int key){
+    int start = 0;
+    int end = size-1;
+    int mid = start + (end-start)/2;
+    int ans = -1;
+
+    while(start <= end){
+        if(arr[mid] == key){
+            ans = mid;
+            start = mid+1;
+        }else if (arr[mid] < key){
+            start = mid+1;
+        }else if(arr[mid] > key){
+            end = mid-1;
+        }
+        mid = start + (end-start)/2;
+    }
+    return ans;
+}
+
+
+
+
+
+
+int main(){
+    int even[5] = {1,2,3,3,5};
+    int a = first(even , 5, 3);
+    cout<<"first occurrence at index : "<<a;
+
+    cout<<"last is : "<<last(even,5,3);
+}
