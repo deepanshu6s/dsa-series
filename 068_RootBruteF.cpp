417766d57329c1ac9a28b50bd78c2d4a5a2dfd18
deepanshu s <deepanshu6sharawat@gmail.com>
2026-05-18 20:40:29 +0530

    added 068

---
+++ 068_RootBruteF.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+int root(int a){
+
+    int i = 0;
+    
+
+}
+int main(){
+    int n;
+    cout<<"Enter the number : ";
+    cin>>n;
+    int b = root(n);
+
+    cout<<"Root is : "<<b;
+
+}
