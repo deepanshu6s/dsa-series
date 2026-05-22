7b1795d1bee56611febba9627fa557a69a564ec8
deepanshu s <deepanshu6sharawat@gmail.com>
2026-05-22 17:55:31 +0530

    aaa

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int i = 1;
+    int n = 5;
+
+    while(i<=n){
+        int j = 1;
+
+        while (j <=i){
+            cout<<j<<" ";
+           
+            j++;
+        } cout<<endl;
+        
+        i++;
+    }
 }
