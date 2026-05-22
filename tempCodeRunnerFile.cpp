5d1d0b471fff7c8162ceb6196d57930662095b8d
deepanshu s <deepanshu6sharawat@gmail.com>
2026-05-22 18:41:30 +0530

    updated some files

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int i = 1;
-    int n = 5;
+
+    int n;
+    cin>>n;
+    int i =1;
+
+    int sum = 0;
 
     while(i<=n){
-        int j = 1;
-
-        while (j <=i){
-            cout<<j<<" ";
-           
-            j++;
-        } cout<<endl;
-        
+        i = i*2;
+        sum = sum+i;
+        i = i/2;
         i++;
     }
-}
+cout<<sum;}
