386fe5b446d944bb0d81775e5f7a649c112f8e5c
deepanshu s <deepanshu6sharawat@gmail.com>
2026-05-25 21:14:06 +0530

    updated something

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,51 +1,73 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int squareRoot(int n){
-    int start = 0;
-    int end = n;
-    int mid = start + (end - start )/2;
-    int ans = -1;
+bool isPossible(vector<int>& arr, int n, int m, int mid){
 
-    while (start < end){
-        int sq = mid*mid;
-        if(sq == n){
-            return mid;
-        }else if(sq < n){
-            ans = mid;
-            start = mid+1;
-        }else {
-            end = mid-1;
-        }mid = start + (end - start )/2;
-        
-    }return ans;
+    int studentCount = 1;
+    int pageSum = 0;
+
+    for(int i = 0; i < n; i++){
+
+        if(pageSum + arr[i] <= mid){
+            pageSum += arr[i];
+        }
+        else{
+
+            studentCount++;
+
+            if(studentCount > m || arr[i] > mid){
+                return false;
+            }
+
+            pageSum = arr[i];
+        }
+    }
+
+    return true;
 }
 
-double setPrecision(int n, int precision , int tempSol){
-    double factor = 1;
-    double ans = tempSol;
+int findPages(vector<int>& arr, int n, int m){
 
-    for(int i =0;i<precision;i++){
+    if(m > n){
+        return -1;
+    }
 
-        factor = factor/10;
+    int start = 0;
+    int sum = 0;
 
-        for(double j = ans; j*j<n ; j= j+factor){
-            ans = j;
+    for(int i = 0; i < n; i++){
+        sum += arr[i];
+    }
+
+    int end = sum;
+    int ans = -1;
+
+    while(start <= end){
+
+        int mid = start + (end - start) / 2;
+
+        if(isPossible(arr, n, m, mid)){
+
+            ans = mid;
+            end = mid - 1;
         }
-    }return ans;
+        else{
+            start = mid + 1;
+        }
+    }
+
+    return ans;
 }
 
 int main(){
 
-    int n;
-    cout<<"Enter the value of n : ";
-    cin>>n;
+    vector<int> arr = {10, 20, 30, 40};
 
-    int tempSol = squareRoot(n);
-    cout<<"Temp solution is : "<<tempSol<<endl;
+    int n = arr.size();
+    int m = 2;
 
-    double a = setPrecision(n,3,tempSol);
+    int ans = findPages(arr, n, m);
 
-    cout<<"Main solution is : " <<a;
-   
+    cout << "Minimum pages : " << ans;
 }
