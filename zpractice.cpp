bc190391c16a9d54447c956d8e3fb03995ffb87c
deep <deepanshu6sharawat@gmail.com>
2026-07-02 19:02:38 +0530

    practice question

--- zpractice.cpp
+++ zpractice.cpp
@@ -1,12 +1,27 @@
 #include <iostream>
 using namespace std;
-int main(){
-    int n; 
-    cin>>n;
 
-    int i = 0; 
-    while(i<=n){
-        cout<<i<<" ";
-        i++;
+
+int powerans (int num){
+
+    if(num%2 == 0){
+        return 1;
+    }else {
+        return 0;
     }
+    
+}
+
+
+int main(){
+     int num ;
+     cin>>num;
+     
+     int a = powerans(num);
+
+     if(a == 1){
+        cout<<num<<" is an even number"<<endl;
+     }else{
+        cout<<num<<" is odd "<<endl;
+     }
 }
