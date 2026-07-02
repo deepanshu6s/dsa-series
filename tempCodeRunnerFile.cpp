879512711920fc14d5e4bda5d5ea6f4fd85a521a
deep <deepanshu6sharawat@gmail.com>
2026-07-02 19:16:46 +0530

    practice question

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,12 +1,29 @@
 #include <iostream>
-#include <array>
 using namespace std;
+
+int fact(int num){
+    int mul = 1;
+    for(int i = 1; i<=num ; i++){
+            mul = mul*i;
+    }return mul;
+}
+
+int nCr(int n , int r){
+    int a = fact(n) / (fact(r) * fact(n-r));
+    return a;
+}
+
 int main(){
-    array<int,5> a = {1,2,3,4,6};
 
-    int b = a.size();
+        int a,b;
+        cout<<"enter n: ";
+        cin>>a;
 
-    for(int i = 0; i<b; i++){
-        cout<<a[i]<<" ";
-    }
+        cout<<"enter r: ";
+        cin>>b;
+
+        int answer = nCr(a,b);
+
+        cout<<"nCr = "<<answer<<endl;
+        
 }
--- zpractice.cpp
+++ zpractice.cpp
@@ -1,27 +1,29 @@
 #include <iostream>
 using namespace std;
 
-
-int powerans (int num){
-
-    if(num%2 == 0){
-        return 1;
-    }else {
-        return 0;
-    }
-    
+int fact(int num){
+    int mul = 1;
+    for(int i = 1; i<=num ; i++){
+            mul = mul*i;
+    }return mul;
 }
 
+int nCr(int n , int r){
+    int a = fact(n) / (fact(r) * fact(n-r));
+    return a;
+}
 
 int main(){
-     int num ;
-     cin>>num;
-     
-     int a = powerans(num);
 
-     if(a == 1){
-        cout<<num<<" is an even number"<<endl;
-     }else{
-        cout<<num<<" is odd "<<endl;
-     }
+        int a,b;
+        cout<<"enter n: ";
+        cin>>a;
+
+        cout<<"enter r: ";
+        cin>>b;
+
+        int answer = nCr(a,b);
+
+        cout<<"nCr = "<<answer<<endl;
+        
 }
