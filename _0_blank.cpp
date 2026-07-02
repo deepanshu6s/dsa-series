25138ca4f3b3d21e027aaf7b869c17631cfc00c6
deep <deepanshu6sharawat@gmail.com>
2026-07-02 09:33:28 +0530

    added something

--- _0_blank.cpp
+++ _0_blank.cpp
@@ -7,16 +7,20 @@ int n;
 cin>>n;
 
 int i = 0; 
-int ans =0;
+int ans = 0;
+
+
 
 while(n != 0){
-    int  bit = n&1;
+    int digit = n%10;
+    if(digit & 1){
+        ans = ans + pow(2,i);
 
-    ans = (bit * pow(10,i))+ans;
-     n = n>>1;
+    }
+    n = n/10;
     i++;
-   
-
+     
 }
 cout<<ans<<endl;
+return 0;
 }
