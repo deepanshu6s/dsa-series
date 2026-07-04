b66b7ff6e9ec0bdd5fbaae199bf8f736aa931a72
deep <deepanshu6sharawat@gmail.com>
2026-07-04 11:25:09 +0530

    added something

--- _0_blank.cpp
+++ _0_blank.cpp
@@ -1,30 +1,7 @@
 #include <iostream>
-#include <math.h>
 using namespace std;
 int main()
 {
-long long int n;
-cin>>n;
-int ans = 0; 
-int i = 0;
-
-
-if(n <= -2147483648 || n>= 2147483647){
-    cout<<"reverse = "<<0 <<endl;
-}else{
-
-while(n != 0 ){
-int digit = n%10;
-
-ans = (ans * 10)+ digit;
-n = n/10;
-i++;
-
-}
-cout<<ans;
-
-
-
-}
+cout<<"hello"<<endl;
 
 }
