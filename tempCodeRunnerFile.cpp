c6768dce45e14600f903229d079c96f435ba0657
deep <deepanshu6sharawat@gmail.com>
2026-07-04 11:03:18 +0530

    added something

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,17 +1,42 @@
 #include <iostream>
 using namespace std;
+
+int binary (int arr[], int size ,int key){
+        int start = 0; 
+        int end = size -1; 
+        int mid = start + (end-start)/2;
+
+
+        while(start<=end){
+                if(arr[mid] == key){
+                        return mid;
+                }
+                else if(key > arr[mid]){
+                        start = mid+1;
+                }else{
+                        end = mid-1;
+                }
+                mid = start + (end-start)/2;
+
+        }
+        return -1;
+
+
+}
 int main(){
 
-        int arr[7] = { 1,1,98,98,999,5,5};
-        int size = 5;
-        int unique = 0;
+        int arr[7] = { 1,2,3,4,5,6,7};
         
-        for(int i = 0; i<size ; i = i+1){
+        int key = 7;
+        
+        int ans = binary(arr, 7 , key);
+                cout<<endl;
 
-                unique = unique^arr[i];
-            
-        
+        if(ans == -1){
+                
+                cout<<"element not found"<<endl;
         }
-  cout<<unique<<endl;
-
+        else{
+        cout<<"index of "<<key<<" is : "<<ans<<endl;
+        }
 }
