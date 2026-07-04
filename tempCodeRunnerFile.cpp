10057a2e0cb3a7124757d1ce7c5d1c95344bfd49
deep <deepanshu6sharawat@gmail.com>
2026-07-04 22:10:47 +0530

    added

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,42 +1,31 @@
 #include <iostream>
 using namespace std;
 
-int binary (int arr[], int size ,int key){
-        int start = 0; 
-        int end = size -1; 
-        int mid = start + (end-start)/2;
+int pivot (int arr[], int size){
+       int start = 0; 
+       int end = size -1;
+       int mid = start + (end - start)/2;
 
-
-        while(start<=end){
-                if(arr[mid] == key){
-                        return mid;
-                }
-                else if(key > arr[mid]){
-                        start = mid+1;
-                }else{
-                        end = mid-1;
-                }
-                mid = start + (end-start)/2;
-
+       while(start< end){
+        if(arr[0] < arr[mid]){
+                start = mid+1;
         }
-        return -1;
-
-
+        else if (arr[0] > arr[mid]){
+                end = mid;
+        }
+        mid = start + (end - start)/2;
+        
 }
+return start;
+}
+
+
 int main(){
 
-        int arr[7] = { 1,2,3,4,5,6,7};
+        int arr[5] = {7,9,11,3,5};
         
-        int key = 7;
-        
-        int ans = binary(arr, 7 , key);
-                cout<<endl;
+        int a = pivot(arr, 5);
 
-        if(ans == -1){
-                
-                cout<<"element not found"<<endl;
-        }
-        else{
-        cout<<"index of "<<key<<" is : "<<ans<<endl;
-        }
+        cout<<"pivot is : "<<a<<endl;
+
 }
