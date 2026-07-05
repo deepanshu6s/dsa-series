a8004d054fbb30ec87efd3cebfbb4c47f0fa2cdd
deep <deepanshu6sharawat@gmail.com>
2026-07-05 16:44:52 +0530

    added something

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,31 +1,7 @@
 #include <iostream>
 using namespace std;
-
-int pivot (int arr[], int size){
-       int start = 0; 
-       int end = size -1;
-       int mid = start + (end - start)/2;
-
-       while(start< end){
-        if(arr[0] < arr[mid]){
-                start = mid+1;
-        }
-        else if (arr[0] > arr[mid]){
-                end = mid;
-        }
-        mid = start + (end - start)/2;
-        
-}
-return start;
-}
-
-
 int main(){
-
-        int arr[5] = {7,9,11,3,5};
-        
-        int a = pivot(arr, 5);
-
-        cout<<"pivot is : "<<a<<endl;
-
+        for(int i = 0; i<n; i++){
+                
+        }
 }
