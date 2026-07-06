3045a80c7fb2a6942cad13deb106ae13b936fb47
deep <deepanshu6sharawat@gmail.com>
2026-07-06 23:48:40 +0530

    array practice stl

--- 072_STL.cpp
+++ 072_STL.cpp
@@ -2,7 +2,14 @@
 #include <array>
 using namespace std;
 int main(){
-    int arr[3] = {1, 2, 3};
 
-    cout<<arr[2]<<endl;
+    array<int, 5> arr1 = {1,2,3,4,5};
+
+    cout<<arr1[4]<<endl;
+    cout<<arr1.at(3)<<endl;
+
+    cout<<"empty wala line: ";
+
+    cout<<arr1.empty();
+    cout<<endl;
 }
