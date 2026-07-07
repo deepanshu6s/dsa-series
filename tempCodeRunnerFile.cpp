29c2ef3089d47200f68ad480360d2f172e32246c
deep <deepanshu6sharawat@gmail.com>
2026-07-07 19:14:18 +0530

    added something

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,23 @@
-    // d.pop_back();
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    
+    vector<int> arr  = { 2, 4 , 6};
+
+    cout<<"Size of array is : "<<arr.size()<<endl; 
+
+    cout<<"whats at index no 2  :  "<<arr.at(2)<<endl;
+
+    
+
+    cout<<"is array empty : "<<arr.empty()<<endl;
+
+
+    cout <<"front element : "<<arr.front()<<endl;
+
+    cout<<"last element : "<<arr.back()<<endl;
+
+}
