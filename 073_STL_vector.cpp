080f5c6e89b4c821b54464660d68433553961a32
deep <deepanshu6sharawat@gmail.com>
2026-07-07 10:50:04 +0530

    added something

--- 073_STL_vector.cpp
+++ 073_STL_vector.cpp
@@ -15,11 +15,21 @@ int main(){
     cout<<deep.capacity()<<endl;
 
     deep.pop_back();
-    deep.push_back(1);
+    deep.push_back(999);
 
     // deep.clear();
 
     for(int i = 0; i< deep.size(); i++){
         cout<<deep.at(i)<<endl;
+    }
+
+
+    vector<int> c(deep);
+
+
+    cout<<"we putted deep inside a new array named c and print it down"<<endl;
+
+    for(int i = 0; i<c.size(); i++){
+        cout << c.at(i)<<endl;
     }
 }
