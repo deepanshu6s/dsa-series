53063803b61ff22adb59d41aaa693012ae584ec4
deep <deepanshu6sharawat@gmail.com>
2026-07-07 09:57:06 +0530

    vector file added

--- 073_STL_vector.cpp
+++ 073_STL_vector.cpp
@@ -8,4 +8,18 @@ using namespace std;
 
 int main(){
 
+
+    vector <int> deep(5);
+
+    cout<<deep.size()<<endl;
+    cout<<deep.capacity()<<endl;
+
+    deep.pop_back();
+    deep.push_back(1);
+
+    // deep.clear();
+
+    for(int i = 0; i< deep.size(); i++){
+        cout<<deep.at(i)<<endl;
+    }
 }
