4484d90d0ddbe617ee2d6f7d10cb09cfdd2c4a49
deep <deepanshu6sharawat@gmail.com>
2026-07-07 09:47:11 +0530

    stl array updated

--- 072_STL.cpp
+++ 072_STL.cpp
@@ -3,10 +3,24 @@
 using namespace std;
 int main(){
 
-    array<int, 500> arr1 = {1,2,3,4,5};
+    array<int, 5> arr1 = {1,2,3,4,5};
 
-    cout<<arr1.back();
-    cout<<endl;
+    cout<<"at operation :  ";
+    cout<<arr1.at(4)<<endl;
+
+    cout<<"back operation :  ";
+    cout<<arr1.back()<<endl;
+
+    cout<<"empty :   ";
+    cout<<arr1.empty()<<endl;
+
+    cout<<"front operation  :  ";
+    cout<<arr1.front()<<endl;
+
+
+    cout<<"size :  ";
+    cout<<arr1.size();
+
 
     return 0;
 }
