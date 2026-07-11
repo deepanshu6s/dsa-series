04b7d90f5697bdbfe2c909d6a7e149645e125f8b
deepanshu6s <deepanshu6sharawat@gmail.com>
2026-07-11 21:16:48 +0530

    added changes in file 86

--- 086_palindromeCaseSensitive.cpp
+++ 086_palindromeCaseSensitive.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 using namespace std;
 
+char toLowerCase(char ch){
+    if(ch >= 'a' && ch <='z'){
+        return ch;
+    }else{
+        char temp = ch - 'A' + 'a';
+        return temp;
+    }
+}
+
+
+
 int length (char name[]){
 
     int i = 0; 
