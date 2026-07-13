23a2de9b8d01870b80ca6fbf54c3a3fcb24c24eb
deepanshu6s <deepanshu6sharawat@gmail.com>
2026-07-13 10:31:27 +0530

    added 2d file

--- 0_0blank.cpp
+++ 0_0blank.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
 using namespace std;
 int main(){
-    cout <<"hello world";  //no enter 
-    cout <<"hii1"<<endl;   //new line character
-    cout <<"hii2"<<'\n';   //new line character
-    cout <<"abc\n";        //new line character
-}
+
+    int x,y,num;
+    
+    cout<<"enter row and col : ";
+    cin>>x>>y;
+
+    int arr[x][y];
+
+for(int i = 0; i<x; i++){
+           for(int j = 0; j<y; j++){
+            cout<<"enter "<< i <<" "<<j <<" : ";
+               cin>>arr[i][j];
+           }
+       }
+
+       cout<<"enter num : ";
+       cin>>num;
+
+
+       for(int i = 0; i<x; i++){
+           for(int j = 0; j<y; j++){
+               if(arr[i][j] == num){
+                cout<<"true";
+                    break;
+               }
+           }
+       }
+       cout<<"false";
+       
+    }
