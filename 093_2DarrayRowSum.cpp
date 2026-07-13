ec79fde2c163636326a9915bc2c10db8e55bfe26
deepanshu6s <deepanshu6sharawat@gmail.com>
2026-07-13 22:19:17 +0530

    added 2d

--- 093_2DarrayRowSum.cpp
+++ 093_2DarrayRowSum.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 
@@ -14,6 +15,25 @@ int main(){
         for(int j = 0; j<4; j++){
             cout<<arr[i][j]<<" ";
         }cout<<endl;
+    }
+
+    vector<int> a;
+
+
+    for( int k = 0; k<3; k++){
+        int count = 0;
+        for(int l = 0; l<4; l++){
+            count = count + arr[k][l];
+        }
+            a.push_back(count);
+    }
+
+    int size = a.size();
+
+    cout<<"length of each row is : ";
+
+    for(int i = 0; i<size; i++){
+        cout<<a[i]<<" ";
     }
 
 }
