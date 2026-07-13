0710a889321e4576eaa25184d5eb17f8c91af079
deepanshu6s <deepanshu6sharawat@gmail.com>
2026-07-13 09:34:36 +0530

    file uploaded

--- 091_2D_Array.cpp
+++ 091_2D_Array.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
 using namespace std;
 int main(){
-    
+    int arr[3][4];
+
+    //taking input
+    for(int i = 0; i<3; i++){
+        for(int j = 0; j<4; j++){
+            cout<<"enter "<<i<<" * "<<j<<" : ";
+            cin>>arr[i][j];
+        }
+    }
+
+    //printing output
+
+    for(int i = 0; i<3; i++){
+        for(int j = 0; j<4; j++){
+            cout<<arr[i][j]<<" ";
+        }cout<<endl;
+    }
 }
