e26da1847e1963d4dde2a0af450b76680eca31c8
deepanshu6s <deepanshu6sharawat@gmail.com>
2026-07-15 17:04:08 +0530

    practice

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,39 +1,13 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
-
-
-
 int main(){
-    int arr[3][4] = {1,2,3,4,5,6,7,8,9,10,11,12};
 
 
-    //printing output
-
-    for(int i = 0; i<3; i++){
-        for(int j = 0; j<4; j++){
-            cout<<arr[i][j]<<" ";
-        }cout<<endl;
-    }
-
-    vector<int> a;
+    vector<int> a = {1,2,3,4,5,6,9,8};
 
 
-    for( int k = 0; k<3; k++){
-        int count = 0;
-        for(int l = 0; l<4; l++){
-            count = count + arr[k][l];
-        }
-            a.push_back(count);
-    }
+    cout<<a.back()-1<<endl;
 
-    int size = a.size();
-
-    cout<<"length of each row is : ";
-
-    for(int i = 0; i<size; i++){
-        cout<<a[i]<<" ";
-    }
 
 }
