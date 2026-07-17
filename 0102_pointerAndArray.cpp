1b19179e9d2748ffb326cd3d4055772e400b53ca
deepanshu6s <deepanshu6sharawat@gmail.com>
2026-07-17 22:15:35 +0530

    updated

--- 0102_pointerAndArray.cpp
+++ 0102_pointerAndArray.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int arr[10];
+    int arr[10] = {1};
 
     //address of first memory block is array name itself
     
     cout<<arr<<endl;
 
     cout<<&arr[0]<<endl;
+
+
+    cout<<arr[1];
 
    
     
