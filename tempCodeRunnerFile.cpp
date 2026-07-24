8b30976f55a18ed45261c6158c6ee14e60b0708f
deepanshu6s <deepanshu6sharawat@gmail.com>
2026-07-24 23:55:38 +0530

    temp updated

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,19 +1,19 @@
-#include <iostream>
-using namespace std;
-void getSum(int arr[]){ //can also write int *arr
+// #include <iostream>
+// using namespace std;
+// void getSum(int arr[]){ //can also write int *arr
 
     
-    cout<<"inside funciton : "<<sizeof(arr);
-    cout<<endl;
+//     cout<<"inside funciton : "<<sizeof(arr);
+//     cout<<endl;
 
 
-}
+// }
 
 
-int main(){
+// int main(){
 
-    int arr[5] = {1,2,3,4,5};
+//     int arr[5] = {1,2,3,4,5};
 
-  getSum(arr);
+//   getSum(arr);
 
-}
+// }
