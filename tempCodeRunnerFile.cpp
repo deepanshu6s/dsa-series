2b50e755fdba47a5ff9d23b5a4fa570f017394e9
deepanshu6s <deepanshu6sharawat@gmail.com>
2026-07-26 19:07:44 +0530

    updated git file

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -5,5 +5,11 @@ int main(){
     int* p1 = &i;
     int** p2 = &p1;
 
+    cout<<"&i -> "<<&i<<endl;
+
+    cout<<" *p2 -> "<<*p2 <<endl;
+    cout<<" **p2 -> "<<**p2 <<endl;
+    cout<<" *p1 -> "<< *p1<<endl;
+    cout<<" p1 -> "<< p1<<endl;
 
 }
