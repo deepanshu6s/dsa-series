4d737bbeb9e6221dc177055b2b89450f9eda0377
deepanshu <deepanshu6sharawat@gmail.com>
2026-07-30 16:43:56 +0530

    added 0118

---
+++ 0118_RecursionArraySum.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace  std;
+int arraySum(int arr[], int size, int sum){
+    if(size == 0){
+        cout<<"sum is : "<<sum<<endl;
+        return 0;
+    }
+
+    sum = sum+arr[0];
+    
+    arraySum(arr+1, size-1,sum);
+
+    
+
+}
+int main(){
+    int arr[5] = {1,2,3,4,5};
+    int size = 5;
+    int sum = 0;
+
+    cout << arraySum(arr, 5, sum);
+}
+
+
+//more better approach 
+// #include <iostream>
+// using namespace  std;
+// int arraySum(int arr[], int size){
+
+//     if(size == 0){
+//         return 0;
+//     }
+//     if(size == 1){
+//         return arr[0];
+//     }
+//    int remaining = arraySum(arr+1, size-1);
+//    int sum = arr[0] +remaining;
+
+
+//    return sum;
+    
+
+// }
+// int main(){
+//     int arr[5] = {1,2,3,4,5};
+//     int size = 5;
+
+//     cout << arraySum(arr, 5);
+
+// }
