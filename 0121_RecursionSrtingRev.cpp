c93cbad39ee024ec53e739eb6e4681b7e8022538
deepanshu <deepanshu6sharawat@gmail.com>
2026-07-31 22:00:36 +0530

    added 0126

--- 0121_RecursionSrtingRev.cpp
+++ 0121_RecursionSrtingRev.cpp
@@ -30,23 +30,3 @@ int main(){
 
 
 
-// #include <iostream>
-// using namespace std;
-// int main(){
-    // int n;
-    // cin>>n;
-    // 
-    // if(n%2 != 0 || n >100 || n<1){
-        // cout<<"NO";
-    // }
-    // else{
-// 
-    // int both = n/2;
-    // if(both >0 && both%2 == 0){
-        // cout<<"YES";
-    // }else{
-        // cout<<"NO";
-    // }
-// }
-// }
-// 
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+
+    for(int i = 0; i<n-1; i++){
+    bool sorted = 1;
+        for(int j = 0; j<n-i-1 ;j++){
+            if(arr[j] > arr[j+1]){
+                swap(arr[j], arr[j+1]);
+                sorted = 0;
+            }
+        }
+        if(sorted == 1){
+            break;
+        }
+    }
+
+    cout << "Sorted Array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
