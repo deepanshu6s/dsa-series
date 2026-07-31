44e3757497db30fa2cffb2509f4bcf778bbc3909
deepanshu <deepanshu6sharawat@gmail.com>
2026-07-31 11:11:51 +0530

    added 0121 file for recursion with string

---
+++ 0121_RecursionSrtingRev.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+void reverse(string &s, int start, int end){
+    //base case
+    if(start > end){
+        return;
+    }
+
+    swap(s[start], s[end]);
+    start++;
+    end--;
+
+    reverse(s,start,end);
+
+}
+
+
+int main(){
+    string s = "asfdg";
+
+    int end = s.length()-1;
+    int start = 0;
+    reverse(s,start,end);
+
+    cout<<"string is : "<<s<<endl;
+}
