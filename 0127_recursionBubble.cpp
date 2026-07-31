e16a037f4480a3bd88f52e4addbf0057a45bccb9
deepanshu <deepanshu6sharawat@gmail.com>
2026-07-31 22:22:01 +0530

    added 0127

---
+++ 0127_recursionBubble.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+void sortArray(int *arr, int size){
+    
+    if(size == 0 || size == 1){
+        return;
+    }
+
+
+    for(int i = 0; i<size ;i++){
+    if(arr[i] > arr[i+1]){
+        swap(arr[i], arr[i+1]);
+    }
+}
+    sortArray(arr, size-1);
+
+}
+int main(){
+    int arr[5] = {2,5,1,6,9};
+
+    sortArray(arr,5);
+
+}
